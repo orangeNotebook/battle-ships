4419e70fb338f990a65ca25018aba44f6ef96f2f
Kacper Kiermasz <>
2021-04-02 18:03:23 +0000

    Ship placing on larger grids WIP

--- player-v-player.h
+++ player-v-player.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <iomanip>
 #include <cstdlib>
+#include <cstring>
 
 
 #include "board-creation.h"
@@ -29,29 +30,110 @@ class Ship {
     string name;   
     int size;
 
-		vector<int> place(vector<vector<string>> board){
+
+		int checkLength(string coords){
+			if(coords.size() == 1){
+				return 0;
+			}else if(coords.size() == 2){
+				return 2;
+			}else if(coords.size() == 3){
+				return 3;
+			}else if(coords.size() == 4){
+				return 4;
+			}
+			return 0;
+		}
+
+		Player place(Player player){
 			vector<int> coordsVect;
-			int alphabetToInt = 0;
+			int k = 0;
 			string coords;
+			char direction;
+			bool notPlaced = true;
+			int shipY;
+			char shipX;
+			string str1 = "";
+			string str2 = "";
+			string charSpace = " ";
+			string boxStr = " |";
 
-			cout << "Boat size: " + to_string(this->size) + "\nWhere would you like to place this boat: ";
-			cin >> coords;
+			cout << "Boat size: " + to_string(this->size) + "\nWhere would you like to place this boat [eg. F2]: ";
+			cin >> (coords);
 
-			char c = coords[0];
+			int length = checkLength(coords);
 
-			string coordsX(1,coords[0]);
+			if(length == 2){
+				shipX = coords[0];
+				shipY = (coords[1] - '0');
 
-			for(int i = 0; i < board.size(); i++){
-				for(int j = 0; j < board[i].size(); j++){
+			}else if(length == 0){
+					cout << "Invalid coordinates, please try again\n";
+					return this->place(player);
+
+			}else if(length == 3){
+					str1 += coords[1];
+					str1 += coords[2];
+
+					str2 += coords[0];
+					str2 += coords[1];
+					if(stoi(str1) >=1 && stoi(str1)  <=player.board.size()){
+						shipY = (stoi(str1));
+						shipX = coords[0];
+
+					}else{
+						shipY = (coords[2] - '0');
+						// shipX = (str2);
+					} 
+				}
+			
+
+	
+
+			string coordsX(1,shipX);
+
+			for(int i = 0; i < player.board.size(); i++){
+				for(int j = 0; j < player.board[i].size(); j++){
 					//cout << "Comparing board: '" + board[i][j] + "' to : '" + (" " + (coords[0])) + "'\n";
-					if((" " + coordsX) == board[i][j]){
-						coordsVect.push_back((coords[1] - '0'));
+					if(player.board[i].size() > 26){			
+   				boxStr = "| S";
+  				charSpace = "  ";
+					}
+					if((charSpace + coordsX) == player.board[i][j]){
+						coordsVect.push_back(shipY);
 						coordsVect.push_back(j);
+						cout << to_string(coordsVect[0]);
+						cout << to_string(coordsVect[1]);
 					}
 				}
 			}
-			
-			return coordsVect;
+
+			cout << "What direction should it face? [L, R, U, D]: ";
+			cin >> direction;
+
+			while(notPlaced){
+				
+				switch(direction){
+					case 'L':
+						player.board[coordsVect[0]][coordsVect[1] - k] = boxStr;
+						if ((coordsVect[1] - (k+1)) == (coordsVect[1] - this->size)){ notPlaced = false; }
+						break;
+					case 'R':
+						player.board[coordsVect[0]][coordsVect[1] + k] = boxStr;
+						if ((coordsVect[1] + (k+1)) == (coordsVect[1] + this->size)){ notPlaced = false; }
+						break;
+					case 'D':
+						player.board[coordsVect[0] + k][coordsVect[1]] = boxStr;
+						if ((coordsVect[0] + (k+1)) == (coordsVect[0] + this->size)){ notPlaced = false; }
+						break;
+					case 'U':
+						player.board[coordsVect[0] - k][coordsVect[1]] = boxStr;
+						if ((coordsVect[0] - (k+1)) == (coordsVect[0] - this->size)){ notPlaced = false; }
+						break;
+				}
+				k++;
+			}
+
+			return player;
 		}
 };
 
@@ -113,20 +195,30 @@ int playerVPlayer(){
 	cout << "Welcome "+ players[0].name + " and " + players[1].name + "\n";
 
 	for(int i = 0; i < players.size(); i++){
+		char choice;
 		players[i].board = emptyBoard;
 		players[i].targetBoard = emptyBoard;
 
 		
 		
 		for(int j = 0; j < ships.size(); j++){
-			// Clear();
+			Clear();
 			cout << players[i].name + "'s Board: \n";
 			printBoard(players[i].board);
 			cout << "\n";
-			vector<int> coords = ships[j].place(players[i].board);
-			players[i].board[coords[0]][coords[1]] = "|S";
+			players[i] = ships[j].place(players[i]);
 			cout << "\n";
 		}
+		Clear();
+		cout << players[i].name + "'s Board: \n";
+		printBoard(players[i].board);
+		cout << "\nAre you happy with your board layout? [Y, N]: ";
+		cin >> choice;
+		if(choice == 'N'){
+			players[i].board = emptyBoard;
+			i--;
+		}
+
 	}
 
 	
